e5f11141529750c02979018aa10239fd9e9a9c98
Vasilii Pustovoit <vasily.pustovoit@mail.utoronto.ca>
2025-07-16 18:00:41 -0400

    Working shaming on all machines with files being created even for failing ones

---
+++ keep_shaming_on_all_machines.sh
@@ -0,0 +1,6 @@
+#/bin/bash
+
+while true; do
+  ./run_shame_detector_on_all_machines.sh
+  sleep 60
+done
--- run_shame_detector_on_all_machines.sh
+++ run_shame_detector_on_all_machines.sh
@@ -1,11 +1,8 @@
 #/bin/bash
 
 machine_names="mussel kingcrab homard lobster calamari shrimp prawn"
-machine_names="mussel kingcrab homard lobster shrimp prawn"
 
 for machine in $machine_names; do
   rm -f "$machine.shame"
-  ssh $machine "cd /home/vpustovoit/INTSHAMER/; ./find_the_worst_user_local.sh"
+  ssh $machine "cd /home/vpustovoit/INTSHAMER/; ./find_the_worst_user_local.sh" &&  echo "Success on $machine"|| echo  "Failed on $machine" && echo "Failed to ssh into $machine" > $machine.shame
 done
-
-#ssh prawn "cd /home/vpustovoit/INTSHAMER/; ./find_the_worst_user_local.sh"
