6297df2517e816214ea275236d147e1fc2af3008
Vasilii Pustovoit <vasily.pustovoit@mail.utoronto.ca>
2025-07-16 18:22:34 -0400

    Fully working implementation, but without full last-person shaming thing

--- keep_shaming_on_all_machines.sh
+++ keep_shaming_on_all_machines.sh
@@ -1,6 +1,7 @@
 #/bin/bash
 
 while true; do
+  date
   ./run_shame_detector_on_all_machines.sh
   sleep 60
 done
--- run_shame_detector_on_all_machines.sh
+++ run_shame_detector_on_all_machines.sh
@@ -1,8 +1,9 @@
 #/bin/bash
 
 machine_names="mussel kingcrab homard lobster calamari shrimp prawn"
+timeout=20
 
 for machine in $machine_names; do
-  rm -f "$machine.shame"
-  ssh $machine "cd /home/vpustovoit/INTSHAMER/; ./find_the_worst_user_local.sh" &&  echo "Success on $machine"|| echo  "Failed on $machine" && echo "Failed to ssh into $machine" > $machine.shame
+  timeout $timeout ssh $machine "cd /home/vpustovoit/INTSHAMER/; ./find_the_worst_user_local.sh" &&  echo "Success on $machine"|| $(echo "Failed on $machine"; echo "Failed to ssh into $machine" > $machine.shame)
+  chmod a+r $machine.shame
 done
